faa83b06a7a3ec2e1ac3b2d2e3c039509572b420
Max Scopp <me@maxscopp.de>
2020-11-19 11:40:01 +0100

    v0.0.1-dev.2

---
+++ src/apis/index.ts
@@ -0,0 +1,3 @@
+export * from './DefaultApi';
+export * from './NewsApi';
+export * from './RantingApi';
---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+export * from './CreateMessageDTO';
+export * from './CreateNews';
+export * from './CreateRantDTO';
+export * from './Credentials';
+export * from './MakeVoteDTO';
+export * from './UserRegistration';
